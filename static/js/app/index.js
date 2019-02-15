f5cceccbbdfd71a066e8f68f80d3807d112e7114
Etienne David <etienne.david@inra.fr>
2019-02-15 17:10:15 +0100

    ...

--- static/js/app/index.js
+++ static/js/app/index.js
@@ -35,10 +35,10 @@ function(Pagination, Viewer, util) {
     var pagination = new Pagination(data.imageURLs.length, params);
     document.body.appendChild(pagination.render());
     document.body.appendChild(createLabelOptions(params, data.labels));
+    var project = document.getElementById("project").content
+
     for (var i = pagination.begin(); i < pagination.end(); ++i) {
-      var project = document.getElementById("project").content
-      console.log(project)
-      var viewer = new Viewer(data.imageURLs[i].replace("data","data/"+ project), data.annotationURLs[i].replace("data","data/"+ project), {
+      var viewer = new Viewer(data.imageURLs[i].replace("data","../data/"+ project), data.annotationURLs[i].replace("data","../data/"+ project), {
                                 width: (params.width || 240),
                                 height: (params.height || 320),
                                 colormap: data.colormap,
